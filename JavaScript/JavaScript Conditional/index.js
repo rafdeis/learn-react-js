fd121140125b750421a635ce23794970b152cb36
rafdeis <abdurrafii002@gmail.com>
2022-10-19 09:30:37 +0700

    Menambahkan Folder JavaScript

---
+++ JavaScript/JavaScript Conditional/index.js
@@ -0,0 +1,64 @@
+// var kalimat = "SanberCode"
+
+// true -->
+
+// if(kalimat == "SanberCode"){
+//     console.log("Keren Cuy!!")
+// }
+
+// false -->
+
+// if(kalimat == "sanbercode"){
+//     console.log("Keren Cuy!!")
+// }
+
+
+
+
+
+// var Internet = "MemilikiKuota"
+
+// true -->
+
+// if(Internet == "MemilikiKuota"){
+//     console.log("Saya bisa bermain game dan Searching Google")
+// } else {
+//     console.log("Saya tidak bisa bermain game dan Searching Google")
+// }
+
+// false -->
+
+// if(Internet == "TidakMemilikiKuota"){
+//     console.log("Saya bisa bermain game dan Searching Google")
+// } else {
+//     console.log("Saya tidak bisa bermain game dan Searching Google")
+// }
+
+
+
+
+
+// Switch Case -->
+
+// var buttonPushed = 1;
+// switch(buttonPushed) {
+//   case 1:   { console.log('matikan TV!'); break; }
+//   case 2:   { console.log('turunkan volume TV!'); break; }
+//   case 3:   { console.log('tingkatkan volume TV!'); break; }
+//   case 4:   { console.log('matikan suara TV!'); break; }
+//   default:  { console.log('Tidak terjadi apa-apa'); }}
+
+
+
+
+// Ternary Operator
+
+var age= 20
+var bisaVote= age > 20 ? "bisa vote" : "belum bisa vote" // hasilnya belum bisa vote
+
+console.log(bisaVote)
+
+var angka = 9
+var jenisBilangan = angka % 2 === 0 ? "Bilangan Genap" : "Bukan Bilangan Genap" //hasilnya bukan bilangan genap
+
+console.log(jenisBilangan)
---
+++ JavaScript/JavaScript ES6/index.js
@@ -0,0 +1,14 @@
+var kalimat = "HaloSanberCode"
+// console.log(kalimat[4])
+
+
+// String properties
+
+// String length
+// console.log(kalimat.length)
+
+// String Method
+
+// charAt
+// console.log(kalimat.charAt(3))
+
