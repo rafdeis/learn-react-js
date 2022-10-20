8e65eeed340589cda647453c321ab2f516f6e14f
rafdeis <abdurrafii002@gmail.com>
2022-10-20 08:40:08 +0700

    Menanmbahkan Folder Looping

---
+++ JavaScript/JavaScript Looping/Looping For/loop.js
@@ -0,0 +1,10 @@
+var angka = 1
+
+while(angka < 10) {
+    console.log(angka)
+    angka++;
+}
+
+for(let angkaFor = 0; angkaFor < 10; angkaFor++){
+    console.log("ini proses for yang ke-", (angkaFor+1))
+}
---
+++ JavaScript/JavaScript Looping/Looping While & For/loop.js
@@ -0,0 +1,20 @@
+// Contoh 1
+
+// var angka = 1
+
+// while(angka < 10) {
+//     console.log(angka)
+//     angka++;
+// }
+
+// Contoh 2
+
+var i = 0;
+while( i < 5){
+    if ( i === 3 ) {
+        console.log("ini while dengan kondisi")
+    } else {
+        console.log(i)
+    }
+    i++;
+}
