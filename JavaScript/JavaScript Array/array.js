3a3bdfad0bbd77bbdf87dcf08c20d59051fae664
rafdeis <abdurrafii002@gmail.com>
2022-10-20 09:28:58 +0700

    Menanmbahkan Folder Array

---
+++ JavaScript/JavaScript Array/array.js
@@ -0,0 +1,64 @@
+// var array = [1, 2, 3]
+// console.log(array[0])
+
+
+// Length
+
+// var array = [1, 2, 3, 13, 14]
+// console.log(array.length)
+
+
+// Method Push
+
+// var array = [1, 2, 3]
+// array.push(12)
+
+// console.log(array)
+
+
+//Method Pop
+
+// var array = [1, 2, 3]
+// array.pop()
+
+// console.log(array)
+
+
+//Method Unshift
+
+// var array = [1, 2, 3]
+// array.unshift(4)
+
+// console.log(array)
+// console.log(array[3])
+
+
+//Method Sort
+
+// var Fav = ["Pubg", "MobileLegend", "Dota", "Valorant"]
+// Fav.sort()
+
+// console.log(Fav)
+
+
+//Method Splice
+
+// var Film = ["Action", "Horror", "Drama"]
+// Film.splice(0, 2, "Comedy")
+
+// console.log(Film)
+
+
+//Method Split & Join
+
+//Split
+// var biodata = "name:john,doe" 
+// console.log(biodata.split(":"))
+
+//Join
+// var title = ["my", "first", "experience", "as", "programmer"] 
+// console.log(title.join(" ")) 
+
+//Digabung
+var biodata = "You:Are Beutiful" 
+console.log(biodata.split(":").join(" "))
---
+++ JavaScript/JavaScript Looping/loop.js
@@ -0,0 +1,34 @@
+// Looping While
+// Contoh 1
+
+// var angka = 1
+
+// while(angka < 10) {
+//     console.log(angka)
+//     angka++;
+// }
+
+// Contoh 2
+
+// var i = 0;
+// while( i < 5){
+//     if ( i === 3 ) {
+//         console.log("ini while dengan kondisi")
+//     } else {
+//         console.log(i)
+//     }
+//     i++;
+// }
+
+// Looping For
+
+var angka = 1
+
+while(angka < 10) {
+    console.log(angka)
+    angka++;
+}
+
+for(let angkaFor = 0; angkaFor < 10; angkaFor++){
+    console.log("ini proses for yang ke-", (angkaFor+1))
+}
