ea92c6a0ee8d2fe0ae8879502fb3e7e6ff81b62e
rafdeis <abdurrafii002@gmail.com>
2022-10-26 20:02:19 +0700

    Mengerjakan Tugas

--- Tugas/tugas-reactjs/src/App.js
+++ Tugas/tugas-reactjs/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import './css/tugas7.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='card'>
+     <h1> Hi, Saya Abdurrafi</h1>
+     <hr/>
+     <p> Saya dari batch 39 sudah berhasil menginstal React</p>
     </div>
   );
 }
