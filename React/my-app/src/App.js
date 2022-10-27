0629fb720c45d8308fb626feea624df8331e05e6
rafdeis <abdurrafii002@gmail.com>
2022-10-27 09:51:40 +0700

    Mempelajari State & Handle

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import './App.css';
 import Welcome from "./welcomeComponent";
 import Button from "./components/button";
 
 const App = () => {
+
+  const [nama, setName] = useState ("Rafi")
+  const [gender, setGender] = useState ("Laki-Laki")
+
+  const handleChangeName = () => {
+    setName(nama === "Rafi"  ?  "Abdurrafi" : "Rafi")
+    setGender(gender === "Laki-Laki"  ?  "Perempuan" : "Laki-Laki")
+  
+  }
+
   return (
     <>
     <div class="App">
-      <h1>Hello</h1>
+      { gender === "Laki-Laki" ? <span>Mr.</span> : <span>Mrs.</span> }
+      <span>{ nama }</span>
+      <button onClick={handleChangeName}> Change Text </button>
+      <hr/>
       <Welcome name="Rafi"/>
       <Button/>
     </div>
