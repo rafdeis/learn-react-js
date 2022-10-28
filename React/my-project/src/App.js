c8e7ec06ffcf4136f49882262d0feeb8afa1b119
rafdeis <abdurrafii002@gmail.com>
2022-10-28 22:30:17 +0700

    Mempelajari flowbite

--- React/my-project/src/App.js
+++ React/my-project/src/App.js
@@ -1,10 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import { Navbar } from 'flowbite-react';
 
 export default function App() {
   return (
         <>
-
+          <Navbar
+  fluid={true}
+  rounded={true}
+>
+  <Navbar.Brand href="https://flowbite.com/">
+    <img
+      src="https://flowbite.com/docs/images/logo.svg"
+      className="mr-3 h-6 sm:h-9"
+      alt="Flowbite Logo"
+    />
+    <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+      Flowbite
+    </span>
+  </Navbar.Brand>
+  <Navbar.Toggle />
+  <Navbar.Collapse>
+    <Navbar.Link
+      href="/navbars"
+      active={true}
+    >
+      Home
+    </Navbar.Link>
+    <Navbar.Link href="/navbars">
+      About
+    </Navbar.Link>
+    <Navbar.Link href="/navbars">
+      Services
+    </Navbar.Link>
+    <Navbar.Link href="/navbars">
+      Pricing
+    </Navbar.Link>
+    <Navbar.Link href="/navbars">
+      Contact
+    </Navbar.Link>
+  </Navbar.Collapse>
+</Navbar>
 
           <div className="bg-gray-50">
             <div className="mx-auto max-w-7xl py-12 px-4 sm:px-6 lg:flex lg:items-center lg:justify-between lg:py-16 lg:px-8">
