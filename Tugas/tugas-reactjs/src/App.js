5a182a7140f80ca88d6087a25dfc88348a05e8ae
rafdeis <abdurrafii002@gmail.com>
2022-10-29 09:57:12 +0700

    Mengerjakan Tugas

--- Tugas/tugas-reactjs/src/App.js
+++ Tugas/tugas-reactjs/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Tugas7 from './tugas7/tugas7';
 import Tugas8 from './tugas8/tugas8';
 import Tugas9 from './tugas9/tugas9';
+import Tugas10 from './tugas10/tugas10';
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
     <Tugas7/>
     <Tugas8 name="Abdurrafi" batch="39" email="abdurrafii002@gmail.com"/>
     <Tugas9/>
+    <Tugas10/>
   </>
   );
 }
---
+++ Tugas/tugas-reactjs/src/tugas10/tugas10.js
@@ -0,0 +1,160 @@
+import React from "react";
+import {Table} from "flowbite-react"
+
+const Tugas10 = () => {
+    return (
+        <>
+<div class="bg-white shadow w-3/6 m-auto text-left text-gray-500 shadow-xl bg-white rounded-2xl mb-20 mt-20 ">
+<Table >
+  <Table.Head className="text-xs bg-purple-500 text-white">
+    <Table.HeadCell>
+      NO
+    </Table.HeadCell>
+    <Table.HeadCell>
+      NAMA
+    </Table.HeadCell>
+    <Table.HeadCell>
+      MATA KULIAH
+    </Table.HeadCell>
+    <Table.HeadCell>
+      NILAI
+    </Table.HeadCell>
+    <Table.HeadCell>
+      INDEX NILAI
+    </Table.HeadCell>
+    <Table.HeadCell>
+      ACTION
+    </Table.HeadCell>
+  </Table.Head>
+  <Table.Body className="divide-y ">
+    <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+      <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+        1
+      </Table.Cell>
+      <Table.Cell>
+        David Alexandres
+      </Table.Cell>
+      <Table.Cell>
+        Algoritma
+      </Table.Cell>
+      <Table.Cell>
+        87
+      </Table.Cell>
+      <Table.Cell>
+        B
+      </Table.Cell>
+      <Table.Cell>
+        <a
+          href="/tables"
+          className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-2 py-2 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+        >
+          Edit
+        </a>
+        <a
+          href="/tables"
+          className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+        >
+          Delete
+        </a>
+      </Table.Cell>
+    </Table.Row>
+    <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+      <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+        2
+      </Table.Cell>
+      <Table.Cell>
+        Mike Alelino
+      </Table.Cell>
+      <Table.Cell>
+        Matematika
+      </Table.Cell>
+      <Table.Cell>
+        76
+      </Table.Cell>
+      <Table.Cell>
+        C
+      </Table.Cell>
+      <Table.Cell>
+        <a
+          href="/tables"
+          className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-2 py-2 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+        >
+          Edit
+        </a>
+        <a
+          href="/tables"
+          className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+        >
+          Delete
+        </a>
+      </Table.Cell>
+    </Table.Row>
+    <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+      <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+        3
+      </Table.Cell>
+      <Table.Cell>
+        Monkey D Luffy
+      </Table.Cell>
+      <Table.Cell>
+        Kimia
+      </Table.Cell>
+      <Table.Cell>
+        98
+      </Table.Cell>
+      <Table.Cell>
+        A
+      </Table.Cell>
+      <Table.Cell>
+        <a
+          href="/tables"
+          className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-2 py-2 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+        >
+          Edit
+        </a>
+        <a
+          href="/tables"
+          className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+        >
+          Delete
+        </a>
+      </Table.Cell>
+    </Table.Row>
+    <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+      <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+        4
+      </Table.Cell>
+      <Table.Cell>
+        Qurtoiz
+      </Table.Cell>
+      <Table.Cell>
+        Fisika
+      </Table.Cell>
+      <Table.Cell>
+        69
+      </Table.Cell>
+      <Table.Cell>
+        C
+      </Table.Cell>
+      <Table.Cell>
+        <a
+          href="/tables"
+          className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-2 py-2 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+        >
+          Edit
+        </a>
+        <a
+          href="/tables"
+          className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+        >
+          Delete
+        </a>
+      </Table.Cell>
+    </Table.Row>
+  </Table.Body>
+</Table>
+</div>
+</>
+    );
+}
+export default Tugas10
