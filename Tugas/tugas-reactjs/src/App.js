3e8e3bdb68dc864c3dbb7b9b3b717fae78d71b31
rafdeis <abdurrafii002@gmail.com>
2022-11-02 17:26:14 +0700

    Menambah tugas13

--- Tugas/tugas-reactjs/src/App.js
+++ Tugas/tugas-reactjs/src/App.js
@@ -5,16 +5,18 @@ import Tugas9 from './tugas9/tugas9';
 import Tugas10 from './tugas10/tugas10';
 import Tugas11 from './tugas11/tugas11';
 import Tugas12 from './tugas12/tugas12';
+import Tugas13 from './tugas13/tugas13';
 
 const App = () => {
   return (
   <>
     {/* <Tugas7/>
     <Tugas8 name="Abdurrafi" batch="39" email="abdurrafii002@gmail.com"/>
-    <Tugas9/>
-    <Tugas10/> */}
-    {/* <Tugas11/> */}
-    <Tugas12/>
+    <Tugas9/> */}
+    {/* <Tugas10/>
+    <Tugas11/> */}
+    {/* <Tugas12/> */}
+    <Tugas13/>
   </>
   );
 }
